b066ec855b4238179e2d0c15b4678ba236815355
Ning GUO <bernard.guo1994@gmail.com>
2021-05-30 01:25:05 +1000

    Add AutoMapper pattern

---
+++ BankAccountApi/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using BankAccountApi.DTOs;
+using BankAccountApi.Entities;
+
+namespace BankAccountApi.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, ReturnedUserDto>();
+            CreateMap<CreateUserDto, AppUser>();
+            CreateMap<BankAccount, ReturnedBankAccountDto>();
+            
+        }
+    }
+}
